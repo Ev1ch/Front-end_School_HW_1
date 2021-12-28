57623c41dedadce4a30feca9a1c68e5507741c56
Vlad Zusko <vladzvu11@gmail.com>
2021-12-28 18:25:25 +0200

    fix: state interfaces fixed

--- src/containers/user/logic/state.ts
+++ src/containers/user/logic/state.ts
@@ -1,18 +1,4 @@
-import { IFeedTikTuk } from 'types/feed';
-import { IUser } from 'types/user';
-
-export interface IUserInformationState {
-  item: IUser | null;
-}
-
-export interface IUserFeedState {
-  items: IFeedTikTuk[] | null;
-  options: {
-    limit: number;
-    pageSize: number;
-    pageNumber: number;
-  };
-}
+import { IUserInformationState, IUserFeedState } from './types';
 
 export interface IUserState {
   isLoading: boolean;
